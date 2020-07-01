2556f461da3357ea4c3eee71e1a8aa1aacb2eeff
Endy Ranaudo <endy@wearesnook.com>
2020-07-01 18:12:58 +0100

    last commit of the day

--- Calculator.rb
+++ Calculator.rb
@@ -4,9 +4,8 @@ require 'pry'
 
 class Calculator
 
-  # def initialize
-  #   @calculator = Calculator.new
-  # end
+  def initialize
+  end
 
   def add (num1, num2)
     num1 + num2
@@ -24,27 +23,27 @@ class Calculator
     num1 / num2
   end
   
-  # def select_operation(num1, num2)
-  #   puts "Type 'add' 'sub' 'mul' 'div' to select an operation"
-  #   # selected = prompt.select(%w(add sub mul div ))
-  #   selected = gets.chomp()
-  #   if selected == "add"
-  #     result = add(num1, num2)
-  #   elsif selected == "sub"
-  #     result = subtract(num1, num2)
-  #   elsif selected == "mul"
-  #     result = multiply(num1, num2)
-  #   else  
-  #     result = divide(num1, num2)
-  #   end
-  # end
+  def select_operation(num1, num2)
+    puts "Type 'add' 'sub' 'mul' 'div' to select an operation"
+    # selected = prompt.select(%w(add sub mul div ))
+    selected = gets.chomp()
+    if selected == "add"
+      result = add(num1, num2)
+    elsif selected == "sub"
+      result = subtract(num1, num2)
+    elsif selected == "mul"
+      result = multiply(num1, num2)
+    else  
+      result = divide(num1, num2)
+    end
+  end
 end
 
 
 
-# puts 'Enter your first number'
-# num1 = gets.chomp().to_i
-# puts 'Enter your second number'
-# num2 = gets.chomp().to_i
-# final_result = select_operation(num1, num2)
-# puts "result: #{final_result}"
+puts 'Enter your first number'
+num1 = gets.chomp().to_i
+puts 'Enter your second number'
+num2 = gets.chomp().to_i
+final_result = select_operation(num1, num2)
+puts "result: #{final_result}"
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,7 +1,7 @@
 class Calculator
 
-  def initialize
-  end
+  # def initialize
+  # end
 
   def add (num1, num2)
     num1 + num2
@@ -23,21 +23,22 @@ end
 
 
 RSpec.describe 'Calculator' do
-  calculator = Calculator.new
+  let (:calculator) {Calculator.new}
+
   it 'adds two numbers' do
     expect(calculator.add(1,2)).to eq(3)
   end
     
-  # it 'subtracts two numbers' do
-  #   expect(calculator.subtract(4,2)).to eq(2)
-  # end
+  it 'subtracts two numbers' do
+    expect(calculator.subtract(4,2)).to eq(2)
+  end
     
-  # it 'multiplies two numbers' do
-  #   expect(calculator.multiply(4,2)).to eq(8)
-  # end
+  it 'multiplies two numbers' do
+    expect(calculator.multiply(4,2)).to eq(8)
+  end
     
-  # it 'divides two numbers' do
-  #   expect(calculator.divide(4,2)).to eq(2)
-  # end
+  it 'divides two numbers' do
+    expect(calculator.divide(4,2)).to eq(2)
+  end
 
 end
